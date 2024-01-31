b2d98625d5ce25ab294c3efeb9430c1d0531960f
root <root@Jagadheesh>
2024-01-31 22:30:20 +0530

    modified one migration file

--- migrations/20240131165812-add-courseId-in-chapter.js
+++ migrations/20240131165812-add-courseId-in-chapter.js
@@ -18,6 +18,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("Chapter", "courseId");
+    await queryInterface.removeColumn("Chapters", "courseId");
   },
 };
