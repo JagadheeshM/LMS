5957963d685f0f7f389eb7aeb6e3cefdf9c8e366
root <root@Jagadheesh>
2024-02-03 23:54:08 +0530

    added bcrypt,authentiation

--- app.js
+++ app.js
@@ -5,6 +5,66 @@ const bodyParser = require("body-parser");
 const path = require("path");
 app.use(bodyParser.json());
 
+//authentication purpose
+
+const passport = require("passport");
+const connectEnsureLogin = require("connect-ensure-login");
+const session = require("express-session");
+const LocalStrategy = require("passport-local");
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+app.use(
+  session({
+    secret: "my-super-secret-key-16060420102002061004",
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    (username, password, done) => {
+      User.findOne({ where: { email: username } })
+        .then(async (user) => {
+          const passRes = await bcrypt.compare(password, user.password);
+          if (passRes) {
+            return done(null, user);
+          } else {
+            return done("Invalid password");
+          }
+        })
+        .catch((error) => {
+          return error;
+        });
+    },
+  ),
+);
+
+passport.serializeUser((user, done) => {
+  console.log("serializing user in session", user.id);
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findByPk(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((error) => {
+      done(error, null);
+    });
+});
+
 //set EJS as view engine
 
 app.set("view engine", "ejs");
@@ -13,14 +73,18 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const { Course, Chapter, Page, User } = require("./models");
 
-app.get("/", async (resquest, response) => {
-  const courses = await Course.getCourses();
-  if (resquest.accepts("html")) {
-    response.render("index", { courses });
-  } else {
-    response.json({ courses });
-  }
-});
+app.get(
+  "/",
+  connectEnsureLogin.ensureLoggedIn(),
+  async (resquest, response) => {
+    const courses = await Course.getCourses();
+    if (resquest.accepts("html")) {
+      response.render("index", { courses });
+    } else {
+      response.json({ courses });
+    }
+  },
+);
 
 app.get("/courses", (request, response) => {
   console.log("List of courses");
@@ -118,15 +182,40 @@ app.get("/login", (request, response) => {
   response.render("login");
 });
 
+app.post(
+  "/session",
+  passport.authenticate("local", { failureRedirect: "/login" }),
+  (request, response) => {
+    response.redirect("/");
+  },
+);
+
+app.get("/signout", (request, response, next) => {
+  request.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    response.redirect("/");
+  });
+});
+
 app.post("/users", async (request, response) => {
+  //Hash the password
+  const hashedPass = await bcrypt.hash(request.body.password, saltRounds);
+
   const user = await User.create({
     firstName: request.body.firstName,
     lastName: request.body.lastName,
     email: request.body.email,
-    password: request.body.password,
+    password: hashedPass,
     type: request.body.type,
   });
-  response.json(user);
+  request.login(user, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    response.redirect("/");
+  });
 });
 
 module.exports = app;
